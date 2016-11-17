09e16fde8f10f260c2235bc35d360e1427c8a094
Nathan Friend <hello@nathanfriend.io>
2016-11-16 22:30:42 -0400

    adding squares to board
    Note: Array.concat is really slow for big arrays and lots of iterations!

--- app/three-dimensional-board/objects/chessboard.service.ts
+++ app/three-dimensional-board/objects/chessboard.service.ts
@@ -1,5 +1,8 @@
 import * as Pieces from './pieces';
 import { ObjService } from '../obj-service/obj.service';
+import { Utility } from '../../utility/utility';
+import { Square } from './square';
+import { BaseObject } from './base-object';
 
 export class ChessBoardService {
 
@@ -7,6 +10,7 @@ export class ChessBoardService {
     public static $inject = ['$log', ObjService.injectionName];
 
     public pieces: Pieces.ChessPiece[] = [];
+    public squares: Square[] = [];
 
     private gl: WebGLRenderingContext;
     private shaderProgram: WebGLProgram;
@@ -24,56 +28,76 @@ export class ChessBoardService {
 
         // black
         for (var i = 0; i < 8; i++) {
-            this.pieces.push(new Pieces.Pawn(this.gl, this.shaderProgram, $V([-7 + (i * 2), 0, -5]), this.objService.objs['pawn'], Pieces.PieceColor.Black));
+            this.pieces.push(new Pieces.Pawn(this.gl, this.shaderProgram, $V([-7 + (i * 2), 0, -5]), this.objService.objs['pawn'], Pieces.PieceTeam.Black));
         }
 
-        this.pieces.push(new Pieces.Rook(this.gl, this.shaderProgram, $V([-7, 0, -7]), this.objService.objs['rook'], Pieces.PieceColor.Black));
-        this.pieces.push(new Pieces.Rook(this.gl, this.shaderProgram, $V([7, 0, -7]), this.objService.objs['rook'], Pieces.PieceColor.Black));
+        this.pieces.push(new Pieces.Rook(this.gl, this.shaderProgram, $V([-7, 0, -7]), this.objService.objs['rook'], Pieces.PieceTeam.Black));
+        this.pieces.push(new Pieces.Rook(this.gl, this.shaderProgram, $V([7, 0, -7]), this.objService.objs['rook'], Pieces.PieceTeam.Black));
 
-        this.pieces.push(new Pieces.Knight(this.gl, this.shaderProgram, $V([-5, 0, -7]), this.objService.objs['knight'], Pieces.PieceColor.Black));
-        this.pieces.push(new Pieces.Knight(this.gl, this.shaderProgram, $V([5, 0, -7]), this.objService.objs['knight'], Pieces.PieceColor.Black));
+        this.pieces.push(new Pieces.Knight(this.gl, this.shaderProgram, $V([-5, 0, -7]), this.objService.objs['knight'], Pieces.PieceTeam.Black));
+        this.pieces.push(new Pieces.Knight(this.gl, this.shaderProgram, $V([5, 0, -7]), this.objService.objs['knight'], Pieces.PieceTeam.Black));
 
-        this.pieces.push(new Pieces.Bishop(this.gl, this.shaderProgram, $V([-3, 0, -7]), this.objService.objs['bishop'], Pieces.PieceColor.Black));
-        this.pieces.push(new Pieces.Bishop(this.gl, this.shaderProgram, $V([3, 0, -7]), this.objService.objs['bishop'], Pieces.PieceColor.Black));
+        this.pieces.push(new Pieces.Bishop(this.gl, this.shaderProgram, $V([-3, 0, -7]), this.objService.objs['bishop'], Pieces.PieceTeam.Black));
+        this.pieces.push(new Pieces.Bishop(this.gl, this.shaderProgram, $V([3, 0, -7]), this.objService.objs['bishop'], Pieces.PieceTeam.Black));
 
-        this.pieces.push(new Pieces.Queen(this.gl, this.shaderProgram, $V([-1, 0, -7]), this.objService.objs['queen'], Pieces.PieceColor.Black));
+        this.pieces.push(new Pieces.Queen(this.gl, this.shaderProgram, $V([-1, 0, -7]), this.objService.objs['queen'], Pieces.PieceTeam.Black));
 
-        this.pieces.push(new Pieces.King(this.gl, this.shaderProgram, $V([1, 0, -7]), this.objService.objs['king'], Pieces.PieceColor.Black));
+        this.pieces.push(new Pieces.King(this.gl, this.shaderProgram, $V([1, 0, -7]), this.objService.objs['king'], Pieces.PieceTeam.Black));
+
+        // all black pieces should be colored black and rotated 180 degrees
+        this.pieces.forEach(p => {
+            p.color = { r: 0, g: 0, b: 0, a: 1 };
+            p.initialModelViewMatrix = Matrix.RotationY(Utility.degreesToRadians(180)).ensure4x4()
+        });
 
         // white
         for (var i = 0; i < 8; i++) {
-            this.pieces.push(new Pieces.Pawn(this.gl, this.shaderProgram, $V([-7 + (i * 2), 0, 5]), this.objService.objs['pawn'], Pieces.PieceColor.White));
+            this.pieces.push(new Pieces.Pawn(this.gl, this.shaderProgram, $V([-7 + (i * 2), 0, 5]), this.objService.objs['pawn'], Pieces.PieceTeam.White));
         }
-        
-        this.pieces.push(new Pieces.Rook(this.gl, this.shaderProgram, $V([-7, 0, 7]), this.objService.objs['rook'], Pieces.PieceColor.White));
-        this.pieces.push(new Pieces.Rook(this.gl, this.shaderProgram, $V([7, 0, 7]), this.objService.objs['rook'], Pieces.PieceColor.White));
 
-        this.pieces.push(new Pieces.Knight(this.gl, this.shaderProgram, $V([-5, 0, 7]), this.objService.objs['knight'], Pieces.PieceColor.White));
-        this.pieces.push(new Pieces.Knight(this.gl, this.shaderProgram, $V([5, 0, 7]), this.objService.objs['knight'], Pieces.PieceColor.White));
+        this.pieces.push(new Pieces.Rook(this.gl, this.shaderProgram, $V([-7, 0, 7]), this.objService.objs['rook'], Pieces.PieceTeam.White));
+        this.pieces.push(new Pieces.Rook(this.gl, this.shaderProgram, $V([7, 0, 7]), this.objService.objs['rook'], Pieces.PieceTeam.White));
 
-        this.pieces.push(new Pieces.Bishop(this.gl, this.shaderProgram, $V([-3, 0, 7]), this.objService.objs['bishop'], Pieces.PieceColor.White));
-        this.pieces.push(new Pieces.Bishop(this.gl, this.shaderProgram, $V([3, 0, 7]), this.objService.objs['bishop'], Pieces.PieceColor.White));
+        this.pieces.push(new Pieces.Knight(this.gl, this.shaderProgram, $V([-5, 0, 7]), this.objService.objs['knight'], Pieces.PieceTeam.White));
+        this.pieces.push(new Pieces.Knight(this.gl, this.shaderProgram, $V([5, 0, 7]), this.objService.objs['knight'], Pieces.PieceTeam.White));
 
-        this.pieces.push(new Pieces.Queen(this.gl, this.shaderProgram, $V([-1, 0, 7]), this.objService.objs['queen'], Pieces.PieceColor.White));
+        this.pieces.push(new Pieces.Bishop(this.gl, this.shaderProgram, $V([-3, 0, 7]), this.objService.objs['bishop'], Pieces.PieceTeam.White));
+        this.pieces.push(new Pieces.Bishop(this.gl, this.shaderProgram, $V([3, 0, 7]), this.objService.objs['bishop'], Pieces.PieceTeam.White));
 
-        this.pieces.push(new Pieces.King(this.gl, this.shaderProgram, $V([1, 0, 7]), this.objService.objs['king'], Pieces.PieceColor.White));
+        this.pieces.push(new Pieces.Queen(this.gl, this.shaderProgram, $V([-1, 0, 7]), this.objService.objs['queen'], Pieces.PieceTeam.White));
+
+        this.pieces.push(new Pieces.King(this.gl, this.shaderProgram, $V([1, 0, 7]), this.objService.objs['king'], Pieces.PieceTeam.White));
+
+        // squares
+        for (var i = 0; i < 8; i++) {
+            for (var j = 0; j < 8; j++) {
+                const location = $V([-7 + 2 * i, 0, -7 + 2 * j]);
+                const square = new Square(this.gl, this.shaderProgram, location, this.objService.objs['square']);
+                square.color = (i + j) % 2 === 0 ? { r: 1, g: 1, b: 1, a: 1 } : { r: 0, g: 0, b: 0, a: 1 };
+                this.squares.push(square);
+            }
+        }
     }
 
     public initializeShaders() {
-        this.pieces.forEach(p => {
+        this.getAllObjects().forEach(p => {
             p.initializeShaders();
         });
     }
 
     public initializeBuffers() {
-        this.pieces.forEach(p => {
+        this.getAllObjects().forEach(p => {
             p.initializeBuffers();
         });
     }
 
     public draw(projection: Matrix, modelView: Matrix) {
-        this.pieces.forEach(p => {
+        this.getAllObjects().forEach(p => {
             p.draw(projection, modelView);
         });
+    }
+
+    private getAllObjects(): BaseObject[] {
+        return (<BaseObject[]>this.pieces).concat(this.squares);
     }
 }
--- app/three-dimensional-board/objects/pieces.ts
+++ app/three-dimensional-board/objects/pieces.ts
@@ -1,80 +1,14 @@
 import { Obj } from '../obj-service/obj-parser';
-import { Utility } from '../../utility/utility';
+import { BaseObject } from './base-object';
 
-export abstract class ChessPiece {
+export class ChessPiece extends BaseObject {
+    public pieceTeam: PieceTeam;
     public type: PieceType; 
 
-    private vertexPositionAttribute: number;
-    private vertexColorAttribute: number;
-    private vertexNormalAttribute: number;
+    constructor(gl: WebGLRenderingContext, shaderProgram: WebGLProgram, location: Vector, obj: Obj, pieceTeam: PieceTeam) {
+        super(gl, shaderProgram, location, obj);
 
-    private vertexPositionBuffer: WebGLBuffer;
-    private vertexColorBuffer: WebGLBuffer;
-    private vertexNormalBuffer: WebGLBuffer;
-    private verticesIndexBuffer: WebGLBuffer;
-
-    constructor(private gl: WebGLRenderingContext, private shaderProgram: WebGLProgram, public location: Vector, public obj: Obj, public color: PieceColor) {
-    }
-
-    public initializeShaders() {
-        this.gl.useProgram(this.shaderProgram);
-
-        this.vertexPositionAttribute = this.gl.getAttribLocation(this.shaderProgram, 'vertexPosition');
-        this.gl.enableVertexAttribArray(this.vertexPositionAttribute);
-        this.vertexColorAttribute = this.gl.getAttribLocation(this.shaderProgram, 'vertexColor');
-        this.gl.enableVertexAttribArray(this.vertexColorAttribute);
-        this.vertexNormalAttribute = this.gl.getAttribLocation(this.shaderProgram, 'vertexNormal');
-        this.gl.enableVertexAttribArray(this.vertexNormalAttribute);
-    }
-
-    public initializeBuffers() {
-        this.gl.useProgram(this.shaderProgram);
-
-        const colors = [];
-        for (var i = 0; i < this.obj.renderData.vertexCoords.length / 3 * 4; i++) {
-            colors.push(this.color === PieceColor.Black ? 0.0 : 1.0);
-        }
-
-        this.vertexPositionBuffer = this.gl.createBuffer();
-        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.vertexPositionBuffer);
-        this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array(this.obj.renderData.vertexCoords), this.gl.STATIC_DRAW);
-
-        this.vertexColorBuffer = this.gl.createBuffer();
-        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.vertexColorBuffer);
-        this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array(colors), this.gl.STATIC_DRAW);
-
-        this.vertexNormalBuffer = this.gl.createBuffer();
-        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.vertexNormalBuffer);
-        this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array(this.obj.renderData.vertexNormals), this.gl.STATIC_DRAW);
-
-        this.verticesIndexBuffer = this.gl.createBuffer();
-        this.gl.bindBuffer(this.gl.ELEMENT_ARRAY_BUFFER, this.verticesIndexBuffer);
-        this.gl.bufferData(this.gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(this.obj.renderData.vertexIndices), this.gl.STATIC_DRAW);
-    }
-
-    public draw(projection: Matrix, modelView: Matrix) {
-        this.gl.useProgram(this.shaderProgram);
-
-        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.vertexPositionBuffer);
-        this.gl.vertexAttribPointer(this.vertexPositionAttribute, 3, this.gl.FLOAT, false, 0, 0);
-        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.vertexColorBuffer);
-        this.gl.vertexAttribPointer(this.vertexColorAttribute, 4, this.gl.FLOAT, false, 0, 0);
-        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.vertexNormalBuffer);
-        this.gl.vertexAttribPointer(this.vertexNormalAttribute, 3, this.gl.FLOAT, false, 0, 0);
-        this.gl.bindBuffer(this.gl.ELEMENT_ARRAY_BUFFER, this.verticesIndexBuffer);
-
-        let projectionUniform = this.gl.getUniformLocation(this.shaderProgram, 'projectionMatrix');
-        this.gl.uniformMatrix4fv(projectionUniform, false, new Float32Array(projection.flatten()));
-
-        let modelViewUniform = this.gl.getUniformLocation(this.shaderProgram, 'modelViewMatrix');
-        let rotationMatrix = this.color === PieceColor.Black ? Matrix.RotationY(Utility.degreesToRadians(180)).ensure4x4() : Matrix.I(4);
-        let translatedMatrix = modelView.multiply(Matrix.Translation(this.location).ensure4x4()).multiply(rotationMatrix);
-        this.gl.uniformMatrix4fv(modelViewUniform, false, new Float32Array(translatedMatrix.flatten()));
-
-        let normalUniform = this.gl.getUniformLocation(this.shaderProgram, 'normalMatrix');
-        this.gl.uniformMatrix4fv(normalUniform, false, new Float32Array(translatedMatrix.inverse().transpose().flatten()));
-
-        this.gl.drawElements(this.gl.TRIANGLES, this.obj.rawData.faces.length * 3, this.gl.UNSIGNED_SHORT, 0);
+        this.pieceTeam = pieceTeam;
     }
 }
 
@@ -106,6 +40,6 @@ export enum PieceType {
     Pawn, Rook, Knight, Bishop, Queen, King
 }
 
-export enum PieceColor {
+export enum PieceTeam {
     Black, White
 }
